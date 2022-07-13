b69167dbdd9c5b37e162aa050e65860466620565
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-13 15:19:48 +0800

    Update train.py

--- train.py
+++ train.py
@@ -318,13 +318,13 @@ def train_cl(model, train_datasets,test_datasets, replay_mode="none", scenario="
           for i in range(1,task):
             ecurrent_test_datasets+= test_datasets[i]
           precs_e_task = evaluate.validate(
-            model, ecurrent_test_datasets, verbose=False, test_size=None, task=task+1, with_exemplars=True,
+            model, ecurrent_test_datasets, verbose=False, test_size=None, task=task, with_exemplars=True,
             allowed_classes=list(range(classes_per_task*(task-1), classes_per_task*(task))) if scenario=="task" else None)
           print(" - Task {}: {:.4f}".format(task , precs_e_task))
       
           print("\n\n Exemplars 1st task EVALUATION RESULTS:")
           precs_e1_task = evaluate.validate(
-            model, test_datasets[0], verbose=False, test_size=None, task=task+1, with_exemplars=True,
+            model, test_datasets[0], verbose=False, test_size=None, task=task, with_exemplars=True,
             allowed_classes=list(range(classes_per_task*(task-1), classes_per_task*(task))) if scenario=="task" else None)
           print(" - Task {}: {:.4f}".format(task, precs_e1_task))
       
