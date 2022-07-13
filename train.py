5db1f1df88e856465e244ef4ec5920c71538d07c
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-13 14:32:28 +0800

    Update train.py

--- train.py
+++ train.py
@@ -288,7 +288,8 @@ def train_cl(model, train_datasets,test_datasets, replay_mode="none", scenario="
         print("\n\n TASK EVALUATION RESULTS:")
         print("\n\n Combination of testsets EVALUATION RESULTS:")
             # to get cumulative task accuracy 
-        print(test_datasets[:task])
+        print(task)
+        print(class(test_datasets[task]))
         current_test_datasets = sum(test_datasets[:task])
         precs_task = evaluate.validate(model, current_test_datasets, verbose=False, test_size=None, with_exemplars=False)
         print(" - Task {}: {:.4f}".format(task + 1, precs_task))
