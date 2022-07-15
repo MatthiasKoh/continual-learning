4d6c0bb0f82f236c3ceeb7886965da07d6aa3fe4
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-15 14:05:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ parser.add_argument('--results-dir', type=str, default='./results', dest='r_dir'
 
 # expirimental task parameters
 task_params = parser.add_argument_group('Task Parameters')
-task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST', 'splitMNIST','CIFAR10'])
+task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST','splitMNIST','CIFAR10', 'ANIMALPART'])
 task_params.add_argument('--scenario', type=str, default='class', choices=['task', 'domain', 'class'])
 task_params.add_argument('--tasks', type=int, help='number of tasks')
 
