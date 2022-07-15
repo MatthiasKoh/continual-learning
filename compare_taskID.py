d4720588f36e724e43c64f8a2839247cf4d8034e
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-15 15:16:27 +0800

    Update compare_taskID.py

--- compare_taskID.py
+++ compare_taskID.py
@@ -19,7 +19,7 @@ parser.add_argument('--results-dir', type=str, default='./results', dest='r_dir'
 
 # expirimental task parameters.
 task_params = parser.add_argument_group('Task Parameters')
-task_params.add_argument('--experiment', type=str, default='permMNIST', choices=['permMNIST', 'splitMNIST','CIFAR10'])
+task_params.add_argument('--experiment', type=str, default='permMNIST', choices=['permMNIST', 'splitMNIST','CIFAR10','ANIMALPART'])
 task_params.add_argument('--tasks', type=int, help='number of tasks')
 
 # specify loss functions to be used
