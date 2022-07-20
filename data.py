114111b13c3f6330b9e5d7d0b25961e212784f79
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-20 12:23:11 +0800

    Update data.py

--- data.py
+++ data.py
@@ -42,13 +42,13 @@ def get_dataset(name, type='train', download=True, capacity=None, permutation=No
     # load data-set
     if name == "animalpart":
         dataset = torchvision.datasets.ImageFolder("/content/drive/My Drive/Data/animalpart/",transform=dataset_transform, target_transform=target_transform)
-        train_size = int(0.7 * len(dataset))
-        test_size = len(dataset) - train_size
-        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-        if type == 'test':
-            dataset = test_dataset
-        else:
-            dataset = train_dataset
+        #train_size = int(0.7 * len(dataset))
+       # test_size = len(dataset) - train_size
+        #train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+       # if type == 'test':
+       #     dataset = test_dataset
+       # else:
+        #    dataset = train_dataset
     else:   
         dataset = dataset_class('{dir}/{name}'.format(dir=dir, name=data_name), train=False if type=='test' else True,
                             download=download, transform=dataset_transform, target_transform=target_transform)
