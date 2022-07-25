bd97151d5207b9cbf54ecaa69ec563c856acc366
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-25 16:25:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -671,7 +671,7 @@ if __name__ == '__main__':
       # -run experiment
       result_list= run(args, verbose=True)
       print(result_list)
-    return result_list
+      result.append(result_list)
 
 
     
