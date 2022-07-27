ce784bb026725afbd71058f9dba8708e7848917d
MatthiasKoh <101622954+MatthiasKoh@users.noreply.github.com>
2022-07-27 15:17:09 +0800

    Update data.py

--- data.py
+++ data.py
@@ -351,7 +351,7 @@ def get_multitask_experiment(name, scenario, tasks, data_dir="./datasets", only_
             test_datasets = []
                 
             for labels in labels_per_task:
-                print("labels start",label)
+                print("labels start",labels)
                 target_transform = transforms.Lambda(
                     lambda y, x=labels[0]: y - x
                 ) if scenario=='domain' else None
